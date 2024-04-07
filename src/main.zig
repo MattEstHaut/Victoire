b837e9be7a36faf58e7fa69002ac079f6a313833
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-04-07 22:31:13 +0200

    feat: add cli for testing

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const perft = @import("perft.zig");
+const victoire = @import("victoire.zig");
 const io = @import("io.zig");
 
 pub fn main() !void {
@@ -9,7 +9,19 @@ pub fn main() !void {
     _ = args.skip();
     const fen = args.next() orelse return error.no_fen;
     const depth = try std.fmt.parseInt(u32, args.next() orelse return error.no_depth, 10);
-
     const board = try io.parsing.board(fen);
-    _ = try perft.perft(board, depth);
+
+    var engine = victoire.Engine.init();
+    defer engine.deinit();
+
+    const t0 = std.time.milliTimestamp();
+    const result = engine.search(board, depth, null);
+    const dt = std.time.milliTimestamp() - t0;
+
+    std.debug.print("{s}: {d} (d={d},t={d}ms)\n", .{
+        io.stringify(result.best_move),
+        result.score,
+        result.depth,
+        dt,
+    });
 }
