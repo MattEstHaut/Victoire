9830966d20d883ef8931b2fa2c046275ada8ffc7
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-04-07 13:20:36 +0200

    fix: board update in next()

--- src/victoire.zig
+++ src/victoire.zig
@@ -6,6 +6,39 @@ const movegen = @import("movegen.zig");
 const transposition = @import("transposition.zig");
 const evaluation = @import("evaluation.zig");
 
+const hasher = transposition.ZobristHasher.init();
+
+const SearchNode = struct {
+    board: chess.Board,
+    depth: u32,
+    ply: u32,
+    alpha: i64,
+    beta: i64,
+    hash: u64,
+
+    pub inline fn root(board: chess.Board, depth: u32) SearchNode {
+        return .{
+            .board = board,
+            .depth = depth,
+            .ply = 0,
+            .alpha = -evaluation.checkmate,
+            .beta = evaluation.checkmate,
+            .hash = hasher.calculate(board),
+        };
+    }
+
+    pub inline fn next(self: SearchNode, move: chess.Move) SearchNode {
+        return .{
+            .board = self.board.copyAndMake(move),
+            .depth = self.depth - 1,
+            .ply = self.ply + 1,
+            .alpha = self.alpha,
+            .beta = self.beta,
+            .hash = hasher.update(self.hash, move),
+        };
+    }
+};
+
 /// The Victory Chess Engine.
 pub const Engine = struct {
     options: struct {} = .{},
