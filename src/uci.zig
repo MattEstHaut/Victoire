cd103e08203eff52fb21ecac2f82b18e6fee16cd
Mattéo Delucchi <matteo.dlcch@gmail.com>
2024-04-07 16:05:07 +0200

    feat: add Engine struct

---
+++ src/uci.zig
@@ -0,0 +1,8 @@
+//! This module contains the UCI engine.
+
+const std = @import("std");
+const io = @import("io.zig");
+const chess = @import("chess.zig");
+const victoire = @import("victoire.zig");
+
+pub const Engine = struct {};
